6eed38f2220ab38430234df078677c94727d1a70
Timotei Adam <timotei.adam@emanuel.ro>
2024-03-04 19:28:45 +0200

    problema 5

---
+++ src/ClientComplex.java
@@ -0,0 +1,18 @@
+
+public class ClientComplex {
+
+	public static void main(String[] args) {
+		
+		Complex c1 = new Complex(3, 7);
+		c1.afisareComplex();
+		
+		Complex c2 = new Complex(2, 5.4);
+		
+		Complex suma = c1.suma(c2);
+		suma.afisareComplex();
+		
+		Complex.numarAfisari();
+		c2.numarAfisari();
+	}
+	
+}
